a78c5eef4fea140300089f5c7f426ff4f7587f10
Dany Houssin <danyhoussin50@gmail.com>
2024-09-10 11:47:53 +0200

    Ajout de la fonctionnalité Scroll Reveal (scrollreveal.min.js / script.js / index.html) ; Modification légère du footer (animation en hover) ; Mise en place du media queries (version mobile) et ajustement de la page web.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,21 @@
 function myFunction() {
     var x = document.getElementById("navbarphone");
-    if (x.style.display === "block") {
+    if (x.style.display === "flex") {
       x.style.display = "none";
     } else {
-      x.style.display = "block";
+      x.style.display = "flex";
     }
   }
 
-  $(document).scroll(function () {
-    var y = $(this).scrollTop();
-    if (y > 200) {
-        $('#care').fadeIn();
-    } else {
-        $('#care').fadeOut();
-    }
+const sr = ScrollReveal({
+    origin:'top',
+    distance:'80px',
+    duration:2500,
+    delay:300,
+})
 
-});
+sr.reveal('main img')
+sr.reveal('.hautdepage, .careAbout', {delay:600})
+sr.reveal('.newsText')
+sr.reveal('#news .container', {delay:800})
+sr.reveal('.list', {interval:100})
